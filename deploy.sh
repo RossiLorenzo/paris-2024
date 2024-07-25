ad1f34d750a387b2689d5bd02cb8386be9cd560d
Lorenzo Rossi <lorenzorossi@fb.com>
2024-07-26 00:21:54 +0200

    update code

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ npm run build
 # First commit the code
 git add -A
 git commit -m 'update code'
-git push -f origin main
+git push origin master
 
 # navigate into the build output directory
 cd dist
