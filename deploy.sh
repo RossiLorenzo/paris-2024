862b466d066cd223af29bd2f6684958bcc829ca9
Lorenzo Rossi <lorenzorossi@fb.com>
2024-07-26 15:41:44 +0200

    update code

--- deploy.sh
+++ deploy.sh
@@ -16,11 +16,7 @@ cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-git branch gh-pages
+gh-pages -d dist
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,10 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicPath: process.env.NODE_ENV === 'production' ? '/paris-2024/' : '/',
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+    }
   },
+  base: '/paris-2024/'
 });
-
